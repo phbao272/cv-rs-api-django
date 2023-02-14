1cd221cd2c8f7cd46cb56b4557a9fe251c98f203
PhamQuocBao <baopq@solashi.com>
2023-02-14 18:35:56 +0700

    cacl tf-idf

--- api/views.py
+++ api/views.py
@@ -1,11 +1,25 @@
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfTransformer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from sklearn.neighbors import NearestNeighbors
-import numpy as np
 
 
 @api_view(['GET'])
-def hello_world(request):
-    print(request)
+def calcTFIDF(request):
 
-    return Response({'message': 'Hello, world!'})
+    dataset = [
+        "AAAA BBB",
+        "CCC"
+    ]
+
+    tfIdfTransformer = TfidfTransformer(use_idf=True)
+    countVectorizer = CountVectorizer()
+    wordCount = countVectorizer.fit_transform(dataset)
+    newTfIdf = tfIdfTransformer.fit_transform(wordCount)
+    df = pd.DataFrame(newTfIdf[0].T.todense(
+    ), index=countVectorizer.get_feature_names_out(), columns=["TF-IDF"])
+    df = df.sort_values('TF-IDF', ascending=False)
+
+    return Response({'idfs': newTfIdf})
